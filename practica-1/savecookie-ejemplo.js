de685327c22a86364d0fb9b0f6002fdf54e45b4c
Jose <jrcajide@users.noreply.github.com>
2015-06-06 15:38:54 +0200

    Create savecookie-ejemplo.js

---
+++ practica-1/savecookie-ejemplo.js
@@ -0,0 +1,9 @@
+<script type="text/javascript"> 
+/*
+Crea un cookie y almacena el "Tradedoubler's unique identifier" 
+*/
+  var d = new Date();
+  d.setTime(d.getTime() + (3600*24*365));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = "tduid=" + "489edf2b468ca1b0367f8d0bbf6ab611" + ";" + expires + ";domain=.xx.mitienda.ovh;path=/";;
+</script>
